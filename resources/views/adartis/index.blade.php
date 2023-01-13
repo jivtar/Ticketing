1eb082fcd7cade4e6efe3084626593c3bbaf02e3
asaniafatma <asaniafatma45@gmail.com>
2023-01-13 14:47:22 +0700

    first commit

--- resources/views/adartis/index.blade.php
+++ resources/views/adartis/index.blade.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-6">            
             <h3 class = "mt-3">Daftar Artis</h3>
-            <a href="/adartis/create" class="btn btn-primary my-3">Tambah Data Artis</a>
+            <a href="/adartis/create" class="btn btn-primary my-3">Tambah Daftar Artis</a>
             <ul class="list-group">
                 @foreach ($adartis as $adartiss)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
